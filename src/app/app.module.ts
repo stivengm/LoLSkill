066fb364f2f1a4fa7df279afaba68f771f7f92b8
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-02-15 16:24:36 -0500

    Creación de componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ChampionsComponent } from './components/champions/champions.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent,
+    BlogComponent,
+    ChampionsComponent,
+    AboutComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
