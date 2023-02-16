24c81f110cce7ceca5fefe1b9eaee016fc846d81
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-02-15 19:03:22 -0500

    Ajuste de dashboard con animación flecha.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  placeHolder = "Summoner name...";
+  ngOnInit() {
+    document.getElementById('landing_page')!.style.setProperty('height', 'calc(' + window.innerHeight +'px - 60px)');
+  }
 }
