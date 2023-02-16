231fba3962398ca7b97cf4f4027785e4f264ccd4
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-02-15 21:28:32 -0500

    Ajuste path.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChampionsComponent } from './components/champions/champions.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'champions',
+    component: ChampionsComponent,
     pathMatch: 'full'
   }
 ];
--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isVisibleSearch = false;
+  ngOnInit() {
+    if (window.location.pathname != '/') {
+      this.isVisibleSearch = true;
+    } else {
+      this.isVisibleSearch = false;
+    }
+  }
 
 }
