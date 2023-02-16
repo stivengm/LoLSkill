1fd59672577e519c6d1ce465bc02e9c06a6fb229
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-02-15 20:26:13 -0500

    Ajuste log.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -17,7 +17,6 @@ export class DashboardComponent implements OnInit {
   }
 
   searchSummonerName() {
-    console.log("Hola mundooooooo");
     localStorage.setItem('summonerName', this.summonerName);
   }
 
