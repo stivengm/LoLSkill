fd0c4154f78c0bdf340c382e81eb26dae945465f
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-11-16 12:15:03 -0500

    FIX - Ajuste de imagen de campeones.

--- src/app/core/models/champions.model.ts
+++ src/app/core/models/champions.model.ts
@@ -6,7 +6,29 @@ export interface ChampionsModel {
 }
 
 export interface data {
+    id: string,
     name: string,
+    "key": "266",
     title: string,
-    version: string
+    version: string,
+    blurb: string,
+    info: Info,
+    image: Image
+}
+
+interface Info {
+    attack: number,
+    defense: number,
+    magic: number,
+    difficulty: number,
+}
+
+interface Image {
+    full: string,
+    sprite: string,
+    group: string,
+    x: number,
+    y: number,
+    w: number,
+    h: number,
 }
