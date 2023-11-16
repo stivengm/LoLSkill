afb4b6477ef683ffe98f1fb13409fbe83053980e
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-11-16 12:50:14 -0500

    FIX - Creación de ruta champions about.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChampionsComponent } from './components/champions/champions.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChampionsAboutComponent } from './components/champions-about/champions-about.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,11 @@ const routes: Routes = [
   {
     path: 'champions',
     component: ChampionsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'champion/:id',
+    component: ChampionsAboutComponent,
     pathMatch: 'full'
   }
 ];
