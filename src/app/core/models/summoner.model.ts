efe34088327988be371616b935d2b5ebc8273405
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-01-30 17:42:06 -0500

    ADD - Add model.

---
+++ src/app/core/models/summoner.model.ts
@@ -0,0 +1,9 @@
+export interface SummonerModel {
+    id: string,
+    accountId: string,
+    name: string,
+    profileIconId: number,
+    puuid: string,
+    revisionDate: number,
+    summonerLevel: number
+}
