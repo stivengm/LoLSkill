7ea766d272d378131285eff534e33143e359f8be
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-01-30 21:51:10 -0500

    FIX - Ajuste de importación.

--- src/app/core/services/get-account.service.ts
+++ src/app/core/services/get-account.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AccountModel } from '../models/account.model';
-import { SummonerModel } from '../models/summoner.model';
+
+import { ENVIRONMENT } from 'src/environments/environment';
+
+import { AccountModel, SummonerModel } from '../models/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetAccountService {
 
-  apiKey = 'RGAPI-e26b8219-d0f7-4308-a349-f05093123266'
-
-  url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/XGameGamePlay0X/LAN?api_key=" + this.apiKey;
+  url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/XGameGamePlay0X/LAN?api_key=" + ENVIRONMENT.apiKey;
 
   constructor(private http: HttpClient) { }
 
@@ -21,17 +21,17 @@ export class GetAccountService {
   }
 
   getSummoner() {
-    var api = "https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/XGameGamePlay0X?api_key=" + this.apiKey;
+    var api = "https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/XGameGamePlay0X?api_key=" + ENVIRONMENT.apiKey;
     return this.http.get<SummonerModel>(api);
   }
 
   getChampionsMaestry() {
-    var api = "https://la1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/dkq7y-7rsgtrvnmyFuGq9emx4Dbg4q412O16SbqY855Fh4J0aSwPA63OC8IBAhlX-R8KlIjYcae82Q?api_key=RGAPI-e26b8219-d0f7-4308-a349-f05093123266";
-
+    var api = "https://la1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/dkq7y-7rsgtrvnmyFuGq9emx4Dbg4q412O16SbqY855Fh4J0aSwPA63OC8IBAhlX-R8KlIjYcae82Q?api_key=" + ENVIRONMENT.apiKey;
+    return this.http.get(api);
   }
 
   getLeague() {
-    var api = "https://la1.api.riotgames.com/lol/league/v4/entries/by-summoner/Anff-N-IQVmc57c47bHkDYWEO5i5bAJ4_jM5g-U6h6H_ag?api_key=" + this.apiKey;
+    var api = "https://la1.api.riotgames.com/lol/league/v4/entries/by-summoner/Anff-N-IQVmc57c47bHkDYWEO5i5bAJ4_jM5g-U6h6H_ag?api_key=" + ENVIRONMENT.apiKey;
   }
 
 
